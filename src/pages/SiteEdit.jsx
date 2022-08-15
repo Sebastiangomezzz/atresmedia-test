adc57366b7190d47cee3d224c519b1629b80df14
Sebuntu <sebastiangomezzz@gmail.com>
2022-08-15 02:14:22 +0200

    fix siteEdit form trying to send void values

--- src/pages/SiteEdit.jsx
+++ src/pages/SiteEdit.jsx
@@ -1,46 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useFetchOneSite } from "../hooks/useFetchOneSite";
 import { useParams, Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useEditSite } from "../hooks/useEditSite";
+import compareObjects from "../utils/compareObjects";
 
 export const SiteEdit = () => {
   const { siteId } = useParams();
-  const { data, loading, error } = useFetchOneSite(siteId);
+  const { data: siteData, loading, error } = useFetchOneSite(siteId);
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm();
-  const [editedData, setEditedData] = useState({ ...data });
+  const [editedData, setEditedData] = useState(siteData);
   const { hookHandleEditSubmit, submitLoading, submitError, submitSuccess } =
     useEditSite();
-  const onSubmit = (formData) => {
-    hookHandleEditSubmit(formData, siteId);
+
+  const onSubmit = (data) => {
+    if (data.name === '') {
+      data.name = siteData.name;
+    }
+    if (data.path === '') {
+      data.path = siteData.path;
+    }
+    if (data.publicPath === '') {
+      data.publicPath = siteData.publicPath;
+    }
+    if (data.key === '') {
+      data.key = siteData.key;
+    }
+    if (data.description === '') {
+      data.description = siteData.description;
+    }
+    hookHandleEditSubmit(data, siteId);
   };
+  
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <input defaultValue={siteData.name} {...register("name")} />
+        <input defaultValue={siteData.path} {...register("path")} />
+        <input defaultValue={siteData.publicPath} {...register("publicPath")} />
+        <input defaultValue={siteData.key} {...register("key")} />
         <input
-          defaultValue={data.name}
-          {...register("name", { required: true })}
-        />
-        <input
-          defaultValue={data.path}
-          {...register("path", { required: true })}
-        />
-        <input
-          defaultValue={data.publicPath}
-          {...register("publicPath", { required: true })}
-        />
-        <input
-          defaultValue={data.key}
-          {...register("key", { required: true })}
-        />
-        <input
-          defaultValue={data.description}
-          {...register("description", { required: true })}
+          defaultValue={siteData.description}
+          {...register("description")}
         />
         <input type="submit" />
       </form>
